6d4fc9c09c4388e414a4b3e93c6f9fef4f5c447e
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-08-17 13:59:06 -0700

    Add functionality in terms of adding symbols to any cell on the gameBoard

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 // square varaible that callss on all classes with squares
-const gameSquares = document.querySelector('.column');
+const gameSquares = document.querySelectorAll('.column');
 // intiale player 1 and 2
 
-let player1 = 'X';
-let player2 = '0';
+let currentPlayer1 = 'X';
+let gameReset = false; // keeps track of game
 // loop the promise of clikcing on every square
-gameSquares.forEach((column) => {
-  // add eventlistener for the click
-  gameSquares.addEventListener('click', (e) => {
+gameSquares.forEach((square) => {
+  square.addEventListener('click', (e) => {
     e.preventDefault();
 
-    body.styles.backgroundColor = 'red';
+    if (square.innerHTML === '' && !gameReset) {
+      //conditional check if cell is empty
+      square.innerHTML = currentPlayer1; // if cell is empty, this adds the symbol of currentPlayer
+
+      square.classList.add(currentPlayer1); // using my DOM element square
+      //this either adds an O or X to my square class list
+
+      // we swap currentPlayer is X with O so we can keep track of which symbol is next
+      if (currentPlayer1 === 'X') currentPlayer1 = 'O';
+      else currentPlayer1 = 'X';
+    }
+
+    // this.classList.add(currentPlayer1.toLocaleLowerCase());
+
+    // if (currentPlayer1 === 'X') currentPlayer1 = 'O';
+    // else currentPlayer1 = 'X';
   });
+  // add eventlistener for the click
 });
 
 console.log(gameSquares);
