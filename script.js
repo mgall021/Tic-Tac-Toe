2b96b6e976819ef9f7ee542d26bbb7d93a3b767c
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-08-17 13:29:28 -0700

    add an click event, intialize player varaiables, attempted to get my boxed to change color when selected

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Helloooo');
+// square varaible that callss on all classes with squares
+const gameSquares = document.querySelector('.column');
+// intiale player 1 and 2
+
+let player1 = 'X';
+let player2 = '0';
+// loop the promise of clikcing on every square
+gameSquares.forEach((column) => {
+  // add eventlistener for the click
+  gameSquares.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    body.styles.backgroundColor = 'red';
+  });
+});
+
+console.log(gameSquares);
