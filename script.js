663783ca4b67f411538832182f2d95221bbff5c7
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-08-18 02:04:07 -0700

    Functionality for checking winner works

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// square varaible that callss on all classes with squares
+// Select all elements with the 'column' class
 const gameSquares = document.querySelectorAll('.column');
-// intiale player 1 and 2
 
+// Initialize player 1
 let currentPlayer1 = 'X';
-let gameReset = false; // keeps track of game
+let gameReset = false; // Keeps track of game state
 
 const winMethods = [
   [1, 2, 3],
   [4, 5, 6],
-  [7, 6, 9],
+  [7, 8, 9], // Fixed the win method [7, 6, 9] to [7, 8, 9]
   [1, 4, 7],
   [2, 5, 8],
   [3, 6, 9],
@@ -16,44 +16,25 @@ const winMethods = [
   [3, 5, 7],
 ];
 
-// loop the promise of clikcing on every square
-
-// class checkWin {
-//   constructor(win, tie) {
-//     this.win = win;
-//     this.tie = tie;
-//   }
-// }
-
 gameSquares.forEach((square) => {
   square.addEventListener('click', (e) => {
     e.preventDefault();
 
     if (square.innerHTML === '' && !gameReset) {
-      //conditional check if cell is empty
-      square.innerHTML = currentPlayer1; // if cell is empty, this adds the symbol of currentPlayer
+      square.innerHTML = currentPlayer1;
+      square.classList.add(currentPlayer1);
 
-      square.classList.add(currentPlayer1); // using my DOM element square
-      //this either adds an O or X to my square class list
+      if (checkWin(currentPlayer1)) {
+        console.log('Player', currentPlayer1, 'wins');
+        gameReset = true;
+      }
+      if (currentPlayer1 === 'X') currentPlayer1 = 'O';
+      else currentPlayer1 = 'X';
     }
-
-    if (checkWin(currentPlayer1)) {
-      console.log('Player', currentPlayer1, 'wins');
-      gameReset = true;
-    }
-    // we swap currentPlayer is X with O so we can keep track of which symbol is next
-    if (currentPlayer1 === 'X') currentPlayer1 = 'O';
-    else currentPlayer1 = 'X';
-
-    // this.classList.add(currentPlayer1.toLocaleLowerCase());
-
-    // if (currentPlayer1 === 'X') currentPlayer1 = 'O';
-    // else currentPlayer1 = 'X';
   });
-  // add eventlistener for the click
 });
 
-function checkWIn(player) {
+function checkWin(player) {
   for (let i = 0; i < winMethods.length; i++) {
     const method = winMethods[i];
     let winner = true;
@@ -72,5 +53,3 @@ function checkWIn(player) {
 }
 
 console.log(gameSquares);
-
-console.log(player);
