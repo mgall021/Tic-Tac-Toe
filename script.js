8569b1043d3661dd22e6057eaf6f7c3297ae1079
Marco Gallegos <mgallegosmontano@ucmerced.edu>
2023-08-18 01:49:35 -0700

    add checkwin function

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const gameSquares = document.querySelectorAll('.column');
 
 let currentPlayer1 = 'X';
 let gameReset = false; // keeps track of game
+
+const winMethods = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 6, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
 // loop the promise of clikcing on every square
+
+// class checkWin {
+//   constructor(win, tie) {
+//     this.win = win;
+//     this.tie = tie;
+//   }
+// }
+
 gameSquares.forEach((square) => {
   square.addEventListener('click', (e) => {
     e.preventDefault();
@@ -15,11 +35,15 @@ gameSquares.forEach((square) => {
 
       square.classList.add(currentPlayer1); // using my DOM element square
       //this either adds an O or X to my square class list
-
-      // we swap currentPlayer is X with O so we can keep track of which symbol is next
-      if (currentPlayer1 === 'X') currentPlayer1 = 'O';
-      else currentPlayer1 = 'X';
     }
+
+    if (checkWin(currentPlayer1)) {
+      console.log('Player', currentPlayer1, 'wins');
+      gameReset = true;
+    }
+    // we swap currentPlayer is X with O so we can keep track of which symbol is next
+    if (currentPlayer1 === 'X') currentPlayer1 = 'O';
+    else currentPlayer1 = 'X';
 
     // this.classList.add(currentPlayer1.toLocaleLowerCase());
 
@@ -29,4 +53,24 @@ gameSquares.forEach((square) => {
   // add eventlistener for the click
 });
 
+function checkWIn(player) {
+  for (let i = 0; i < winMethods.length; i++) {
+    const method = winMethods[i];
+    let winner = true;
+    for (let j = 0; j < method.length; j++) {
+      const cell = method[j];
+      if (!gameSquares[cell - 1].classList.contains(player)) {
+        winner = false;
+        break;
+      }
+    }
+    if (winner) {
+      return true;
+    }
+  }
+  return false;
+}
+
 console.log(gameSquares);
+
+console.log(player);
